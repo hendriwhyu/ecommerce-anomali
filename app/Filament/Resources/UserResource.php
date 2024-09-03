598c68b331b27dcba4f37f37d5278abe3863fcdc
Hendri Wahyu Perdana <hendriwp123@gmail.com>
2024-09-03 17:21:49 +0700

    add: resource User, delete: user panel

---
+++ app/Filament/Resources/UserResource.php
@@ -0,0 +1,87 @@
+<?php
+
+namespace App\Filament\Resources;
+
+use App\Filament\Resources\UserResource\Pages;
+use App\Filament\Resources\UserResource\RelationManagers;
+use App\Models\User;
+use Filament\Forms;
+use Filament\Forms\Components\TextInput;
+use Filament\Forms\Form;
+use Filament\Resources\Resource;
+use Filament\Tables;
+use Filament\Tables\Table;
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\SoftDeletingScope;
+
+class UserResource extends Resource
+{
+    protected static ?string $model = User::class;
+
+    protected static ?string $navigationGroup = 'User Management';
+
+    protected static ?string $navigationIcon = 'heroicon-o-user-group';
+
+    public static function form(Form $form): Form
+    {
+        return $form
+            ->schema([
+                Forms\Components\TextInput::make('name'),
+
+                Forms\Components\TextInput::make('email'),
+
+                Forms\Components\TextInput::make('password')
+                ->type('password')
+                ->rules(['required', 'min:8', 'max:255', 'confirmed'])
+                ->label('Password'),
+
+                Forms\Components\TextInput::make('confirm_password')
+                ->type('password')
+                ->label('Confirm Password')
+                ->rules(['required']),
+
+                Forms\Components\Select::make('roles')
+                ->relationship('roles', 'name')
+                ->columnSpan('full'),
+            ]);
+    }
+
+    public static function table(Table $table): Table
+    {
+        return $table
+            ->columns([
+                Tables\Columns\TextColumn::make('name')
+                ->searchable(),
+                Tables\Columns\TextColumn::make('email')
+                ->searchable(),
+                Tables\Columns\TextColumn::make('roles.name')
+                ->badge()
+                ->searchable(),
+                Tables\Columns\TextColumn::make('created_at')
+                ->dateTime(),
+                Tables\Columns\TextColumn::make('updated_at')
+                ->dateTime()
+                ->label('Last Modified')
+            ])
+            ->filters([
+                Tables\Filters\SelectFilter::make('roles')
+                    ->relationship('roles', 'name'),
+            ])
+            ->actions([
+                Tables\Actions\EditAction::make(),
+                Tables\Actions\DeleteAction::make(),
+            ])
+            ->bulkActions([
+                Tables\Actions\BulkActionGroup::make([
+                    Tables\Actions\DeleteBulkAction::make(),
+                ]),
+            ]);
+    }
+
+    public static function getPages(): array
+    {
+        return [
+            'index' => Pages\ManageUsers::route('/'),
+        ];
+    }
+}
--- app/Models/User.php
+++ app/Models/User.php
@@ -50,6 +50,11 @@ class User extends Authenticatable implements FilamentUser
         ];
     }
 
+    public function setPassword(string $password): static{
+        $this->password = bcrypt($password, env('BCRYPT_ROUNDS'));
+        return $this;
+    }
+
     public function canAccessPanel(Panel $panel): bool
     {
         return str_ends_with($this->email, '@anomali.com');
